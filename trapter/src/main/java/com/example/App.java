4764b00521de09fca2c5a85639448b66b7675f5e
BencsikBoglárBianka <bencsikboglarbianka@ktch.hu>
2024-11-14 16:22:42 +0100

    base

---
+++ trapter/src/main/java/com/example/App.java
@@ -0,0 +1,45 @@
+/*
+* File: App.java
+* Author: Bencsik Boglár Bianka
+* Copyright: 2024, Bencsik Boglár Bianka
+* Group: Szoft II/I/N
+* Date: 2024-11-14
+* Github: https://github.com/BencsikBoglarBianka/trapter
+* Licenc: GNU GPL
+*/
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("FirstScene"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
